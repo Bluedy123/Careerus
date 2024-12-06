731c49fad7cdd6fef8b96a8a526907e045fa8ba1
Bluedy123 <Vannak.Nget01@student.csulb.edu>
2024-12-05 16:52:07 -0800

    add login and register
    Add components for login and register authentication

---
+++ components/auth/RegisterForm.js
@@ -0,0 +1,11 @@
+export default function RegisterForm() {
+    return (
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
